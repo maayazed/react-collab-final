bb2406e7bec2f06060a9bc5ffb1d9a091bf13bb2
maayazed <mayahzebrasky@gmail.com>
2021-09-29 22:07:30 -0500

    fixed id context. stored awaiting models as a returned variable. only applies to add and remove book
    Will push later client side fixes. DO NOT DELETE BRANCH

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,86 +4,88 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
 
-    Query: {
-        user: async (parent, { userId }) => {
-          return User.findOne({ _id: userId });
-        },
-
-        users: async () => User.find(),
-
-        library: async (parent, { libraryId }) => {
-          return Library.findOne({ _id: libraryId });
-        },
-        
-        libraries: async ()=> Library.find(),
-
+  Query: {
+    user: async (parent, { userId }) => {
+      return User.findOne({ _id: userId });
     },
 
-    Mutation: {
-          addUser: async (parent, { email, password }) => {
-            const user = await User.create({ email, password });
-            const token = signToken(user);
-            return { token, user };
-          },
+    users: async () => User.find(),
 
-          addLibrary: async (parent, { location, currentBooks }) => {
-            const library = await Library.create({ location, currentBooks });
-            return { library };
-          },
+    library: async (parent, { libraryId }) => {
+      return Library.findOne({ _id: libraryId });
+    },
 
-          addBook: async (parent, { bookId, libraryId, title, authors, description, image, link }, context) => {
-            if (context.user) {
-              return Library.findOneAndUpdate(
-                { _id: libraryId },
-                {
-                  $addToSet: {
-                    currentBooks: { bookId, title, authors, description, image, link },
-                  },
-                },
-                {
-                  new: true,
-                  runValidators: true,
-                }
-              );
-            }
-            throw new AuthenticationError('You need to be logged in!');
-          },
+    libraries: async () => Library.find(),
 
-          login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
-      
-            if (!user) {
-              throw new AuthenticationError('No user found with this email address');
-            }
-      
-            const correctPw = await user.isCorrectPassword(password);
-      
-            if (!correctPw) {
-              throw new AuthenticationError('Incorrect credentials');
-            }
-      
-            const token = signToken(user);
-      
-            return { token, user };
-          },
+  },
 
-          removeBook: async (parent, { libraryId, bookId }, context) => {
-            if (context.user) {
-              return Library.findOneAndUpdate(
-                { _id: libraryId },
-                {
-                  $pull: {
-                    currentBooks: {
-                      bookId: bookId,
-                    },
-                  },
-                },
-                { new: true }
-              );
-            }
-            throw new AuthenticationError('You need to be logged in!');
-          }, 
-    }
+  Mutation: {
+    addUser: async (parent, { email, password }) => {
+      const user = await User.create({ email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addLibrary: async (parent, { location, currentBooks }) => {
+      const library = await Library.create({ location, currentBooks });
+      return { library };
+    },
+
+    addBook: async (parent, { bookId, title, authors, description, image, link }, context) => {
+      if (context.user) {
+        const updateLibrary = await Library.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $addToSet: {
+              currentBooks: { bookId, title, authors, description, image, link },
+            },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+        return updateLibrary;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updateLibrary = await Library.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $pull: {
+              currentBooks: {
+                bookId
+              },
+            },
+          },
+          { new: true }
+        );
+        return updateLibrary;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  }
 };
 
 module.exports = resolvers;
